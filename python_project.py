3a5c2f76308b52fbf6229a8697e5b4d37d0674b0
maciexu <43970987+maciexu@users.noreply.github.com>
2023-09-03 21:34:19 -0400

    Update python_project.py

--- python_project.py
+++ python_project.py
@@ -1,1 +1,38 @@
+# Task 1.  Bitcoin and Cryptocurrencies: Full dataset, filtering, and reproducibility
+# Load the saved CSV file and select relevant columns.
+
+
+# Importing pandas
+import pandas as pd
+
+# Importing matplotlib and setting aesthetics for plotting later.
+import matplotlib.pyplot as plt
+%matplotlib inline
+%config InlineBackend.figure_format = 'svg' 
+plt.style.use('fivethirtyeight')
+
+# Reading datasets/coinmarketcap_06122017.csv into pandas
+dec6 = pd.read_csv('datasets/coinmarketcap_06122017.csv')
+
+# Selecting the 'id' and the 'market_cap_usd' columns
+market_cap_raw = dec6[['id', 'market_cap_usd']]
+
+# Counting the number of values
+market_cap_raw.count()
+
+# Reading in current data from coinmarketcap.com
+current = pd.read_json("https://api.coinmarketcap.com/v1/ticker/")
+
+# Printing out the first few lines
+# ... YOUR CODE FOR TASK 1 ...
+current.head()
+
+
+
+
+
+
+
+
+
 
